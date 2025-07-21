6a301988f6fd88aec9d6082402bcff1567592405
assort_C2\assort_st2 <yuzio2326@gmail.com>
2025-07-21 17:29:13 +0900

    한글 깨짐

--- ServerLearn/Source/ServerLearn/Public/Character/Attribute/BaseCharacterAttributeSet.h
+++ ServerLearn/Source/ServerLearn/Public/Character/Attribute/BaseCharacterAttributeSet.h
@@ -16,29 +16,24 @@ class UBaseCharacterAttributeSet : public UAttributeSet
 	GENERATED_BODY()
 
 	/* 
-	* 각 요소들은 Server에서 계산해야 핵 방지 가능 
-	* 그러면 몬스터 위치도 전부 Server에 보내야 하나?
+	* Each Of Things Have To Cal in Server Reson why? Client Edit Cheater
+	* So Should I Replicate All monster's Position?
 	*/
 
-	// 체력
+
+
 	UPROPERTY(VisibleAnywhere, Replicated)
 	FGameplayAttributeData HP;
-	// 최대 체력
 	UPROPERTY(VisibleAnywhere, Replicated)
 	FGameplayAttributeData MaxHP;
-	// MP
 	UPROPERTY(VisibleAnywhere, Replicated)
 	FGameplayAttributeData MP;
-	// MaxMP
 	UPROPERTY(VisibleAnywhere, Replicated)
 	FGameplayAttributeData MaxMP;
-	// 공격력
 	UPROPERTY(VisibleAnywhere, Replicated)
 	FGameplayAttributeData ATK;
-	// 방어력
 	UPROPERTY(VisibleAnywhere, Replicated)
 	FGameplayAttributeData DEF;
-	// Speed
 	UPROPERTY(VisibleAnywhere, Replicated)
 	FGameplayAttributeData Speed;
 
